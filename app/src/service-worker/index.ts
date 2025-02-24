e0cad776f3af64e58bc14ff826844a789e7a0037
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2025-02-23 23:53:31 -0500

    try: initialize sqlite in sw, will probably fail

--- app/src/service-worker/index.ts
+++ app/src/service-worker/index.ts
@@ -3,12 +3,45 @@
 /// <reference lib="esnext" />
 /// <reference lib="webworker" />
 
+const sw = self as unknown as ServiceWorkerGlobalScope;
+// import sqlite3InitModule from '@sqlite.org/sqlite-wasm';
+
 import { build, files, version } from '$service-worker';
 
-const sw = self as unknown as ServiceWorkerGlobalScope;
+const initializeSqlite = async () => {
+	console.log('Loading and initializing SQLite3 module...');
+	// const sqlite3 = await sqlite3InitModule({
+	// 	print: console.log,
+	// 	printErr: console.error,
+	// })
 
-sw.addEventListener('install', (event) => {
+	// console.log('Running SQLite3 version', sqlite3.version.libVersion);
+	// const db = new sqlite3.oo1.OpfsDb('/mydb.sqlite3');
+
+	// return { db, sqlite3 };
+}
+
+
+sw.addEventListener('install', async (event) => {
 	console.log("installed", event);
+
+	console.log("initiate sqlite3")
+	const sqlite3InitializationPromise = await initializeSqlite();
+	// const { sqlite3, db } = sqlite3InitializationPromise;
+
+	// const tableNames = new Set<string>();
+	// db.exec({
+	// 	sql: `
+    //         SELECT name 
+    //         FROM sqlite_master 
+    //         WHERE type='table'
+    //     `,
+	// 	callback: ([name]: [string]) => {
+	// 		tableNames.add(name);
+	// 	}
+	// });
+
+	// console.log(tableNames)
 });
 
 sw.addEventListener('activate', (event) => {
