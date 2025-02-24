efb030b1773d48d33f968db11ca1f3b6461a5829
Michael-Andrew Keays <michaelandrewkeays@gmail.com>
2025-02-23 23:14:24 -0500

    Try forcing headers at svelte-kit level

---
+++ app/src/routes/+layout.ts
@@ -0,0 +1,7 @@
+import { dev } from '$app/environment';
+
+// navigator.serviceWorker?.register('./src/service-worker/index.ts', {
+// 	type: dev ? 'module' : 'classic'
+// });
+
+// export const ssr = false;
